b0ea37b15b8e517cc2d5f319a5de3ad5d9799130
Diego Siqueira <dsi@hellofresh.com>
2017-09-05 11:53:56 +0200

    Remove old db from context, remove middleware and create logger input

--- context/context.go
+++ context/context.go
@@ -2,11 +2,8 @@ package context
 
 import (
 	"context"
-	"database/sql"
 	"errors"
-	"net/http"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,7 +11,6 @@ type indexContext int
 
 const (
 	traceIDKey = iota
-	dbKey
 )
 
 var (
@@ -22,44 +18,31 @@ var (
 )
 
 func init() {
-	l := logrus.New()
-	l.Formatter = &logrus.JSONFormatter{FieldMap: logrus.FieldMap{
-		logrus.FieldKeyTime: "@timestamp",
-		logrus.FieldKeyMsg:  "message",
-	}}
-
-	fieldLogger = l.WithField("key", "val")
+	fieldLogger = logrus.New().WithFields(nil)
 }
 
-func WithTraceID() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			traceID := r.Header.Get("ot-tracer-traceid")
-			if traceID != "" {
-				ctx = context.WithValue(ctx, traceIDKey, traceID)
-			}
-			next.ServeHTTP(w, r.WithContext(ctx))
-		}
-		return http.HandlerFunc(fn)
+func SetLogger(logger logrus.FieldLogger) {
+	fieldLogger = logger
+}
+
+func WithTraceID(ctx context.Context, traceID string) context.Context {
+	if traceID != "" {
+		ctx = context.WithValue(ctx, traceIDKey, traceID)
 	}
-}
-
-func WithDatabase(db *sql.DB) func(next http.Handler) http.Handler {
-	return middleware.WithValue(dbKey, db)
+	return ctx
 }
 
 func Logger(ctx context.Context) logrus.FieldLogger {
 	logger := fieldLogger
 
-	if traceID, ok := ctx.Value(traceIDKey).(string); ok {
-		logger = logger.WithField("traceID", traceID)
+	if id, err := traceID(ctx); err == nil {
+		logger = logger.WithField("traceID", id)
 	}
 
 	return logger
 }
 
-func TraceID(ctx context.Context) (string, error) {
+func traceID(ctx context.Context) (string, error) {
 	traceID, ok := ctx.Value(traceIDKey).(string)
 	if !ok {
 		return "", errors.New("No TraceID in the context")
@@ -68,11 +51,12 @@ func TraceID(ctx context.Context) (string, error) {
 	return traceID, nil
 }
 
-func DB(ctx context.Context) (*sql.DB, error) {
-	db, ok := ctx.Value(dbKey).(*sql.DB)
-	if !ok {
-		return nil, errors.New("No Database in the context")
+func CheckTimeout(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
 	}
-
-	return db, nil
+	return false
 }
